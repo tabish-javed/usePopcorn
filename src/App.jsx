8e18faefacaf26a15fc552fe4d05bc5d8aeb0080
TABISH <tabishjaved@hotmail.com>
2024-07-05 14:01:57 +0530

    implemented props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -51,30 +51,32 @@ const tempWatchedData = [
 const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App () {
+  const [movies, setMovies] = useState(tempMovieData);
+
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={ movies } />
+      <Main movies={ movies } />
     </>
   );
 }
 
 
-function NavBar () {
+function NavBar ({ movies }) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={ movies } />
     </nav>
   )
 }
 
 
-function NumResults () {
+function NumResults ({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{ movies.length }</strong> results
     </p>
   )
 }
@@ -104,17 +106,17 @@ function Search () {
 }
 
 
-function Main () {
+function Main ({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={ movies } />
       <WatchedBox />
     </main>
   )
 }
 
 
-function ListBox () {
+function ListBox ({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -122,14 +124,13 @@ function ListBox () {
         { isOpen1 ? "–" : "+" }
       </button>
 
-      { isOpen1 && <MovieList /> }
+      { isOpen1 && <MovieList movies={ movies } /> }
     </div>
   )
 }
 
 
-function MovieList () {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList ({ movies }) {
   return (
     <ul className="list">
       { movies?.map((movie) => <Movie movie={ movie } key={ movie.imdbID } />) }
@@ -203,6 +204,7 @@ function WatchedSummary ({ watched }) {
     </div>
   )
 }
+
 
 function WatchedMoviesList ({ watched }) {
   return (
