60bdc712f55132a1cb3f19fe5b56c139f329a54d
TABISH <tabishjaved@hotmail.com>
2024-08-02 12:46:36 +0530

    back to App.jsx (original app)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Challange from "./Challange"
+// import Challange from "./Challange"
 // import StarRating from './StarRating';
 
 
@@ -20,7 +20,7 @@ import Challange from "./Challange"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Challange />
+    <App />
     {/* <StarRating maxRating={ 5 } messages={ ["Terrible", "Bad", "Okay", "Good", "Amazing"] } />
     <StarRating size={ 24 } color='red' className="test" defaultRating={ 3 } />
     <Test /> */}
